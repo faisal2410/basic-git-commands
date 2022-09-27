b36deb2167653333209bd895644170076bfcd859
Faisal Ahmed <faisal2410@yahoo.com>
2022-09-27 09:45:15 +0600

    initial commit

---
+++ __note.js
@@ -0,0 +1,38 @@
+/**
+ * For New Repository (6 commands)
+ * ============
+ * git init
+ * git add .
+ * git commit -m "initial commit"
+ * git branch -M master
+ * git remote add origin https://github.com/faisal2410/basic-git-commands.git * 
+ * git push -u origin master
+ * 
+ * For existing repository (3 commands)
+ * =======================
+ * git add .
+ * git commit -m "2nd commit"
+ * git push
+ * 
+ * */ 
+
+/**
+ * Common git commands
+ * =========================
+ * Un staging File
+ * ==============
+ * git reset Head readme.md
+ * 
+ * git log
+ * 
+ * git log --patch
+ * 
+ * git status
+ * 
+ * Creating an empty folder
+ * ======================
+ * touch temp/.gitkeep
+ * 
+ * Removing file
+ * ============
+ * */ 
